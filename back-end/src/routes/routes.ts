8335ae058e38fdd112c5c893646e3b4e92757635
Brenno Kayan R. de Souza <72401023+brennokayan@users.noreply.github.com>
2025-02-03 10:47:03 -0400

    update: Atualizando o arquivo routes para inserir as rotas de Classe de Lancamento

--- back-end/src/routes/routes.ts
+++ back-end/src/routes/routes.ts
@@ -3,10 +3,12 @@ import { UsuarioRoutes } from "./usuario";
 import { LucroRoutes } from "./lucros";
 import { GastosRoutes } from "./gastos";
 import { Authentication } from "./login";
+import { ClasseLancamentoRoutes } from "./classeLancamento";
 
 export async function Routes(app: FastifyInstance){
     app.register(UsuarioRoutes);
     app.register(LucroRoutes);
     app.register(GastosRoutes);
     app.register(Authentication);
+    app.register(ClasseLancamentoRoutes);
 }
